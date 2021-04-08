d4b486dbe967af74c0ecaeeabde6c0fb7713db26
Mervap <valeratep@mail.ru>
2021-04-08 14:32:19 +0300

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-ipynb=$1
 pdf=$2
 template=$3
-if [ -f $ipynb ]
+if [ -f $1 ]
 then
     if [ -z $template ]
     then
         echo "::group::Ipynb -> Pdf"
         echo "No template"
-        jupyter nbconvert --to=pdf --TemplateExporter.exclude_input=True $1 --output $2.pdf
+        jupyter nbconvert --to=pdf --TemplateExporter.exclude_input=True $1
+        echo "::endgroup::"
     else
-        if [ -f $ipynb ]
-        then
-            echo "::group::Ipynb -> Latex"
-            echo "Using $3 as latex template"
-            jupyter nbconvert --to=latex --LatexExporter.template_file=$3 --TemplateExporter.exclude_input=True $1 --output $2.tex
-            echo "::endgroup::"
-            echo "::group::Latex -> Pdf"
-            pdflatex $2.tex -halt-on-error -jobname $2
-        else
-            echo "::error ::$template template file not found"
-            exit 1
-        fi
+        workingDir=$(dirname $1)
+        ipynb=$(basename $1)
+        ipynbName="${ipynb%.*}"
+        echo "::group::Ipynb -> Latex"
+        echo "Using $3 as latex template"
+        jupyter nbconvert --to=latex --LatexExporter.template_file=$3 --TemplateExporter.exclude_input=True $1
+        echo "::endgroup::"
+        echo "::group::Latex -> Pdf"
+        cd $workingDir && pdflatex $ipynbName.tex -halt-on-error && mv $ipynbName.pdf $2.pdf
+        echo "::endgroup::"
     fi
-    echo "::endgroup::"
 else
-    echo "::error ::$ipynb source file not found"
+    echo "::error ::$1 source file not found"
     exit 1
 fi
